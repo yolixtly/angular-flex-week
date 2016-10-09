1e7cd33da3ab90c38eeae79baba15dfa403353c4
yolixtly <yolixtly_13@hotmail.com>
2016-10-09 16:58:44 -0400

    Tada!! Simple app done

--- fundamentals/yoli-todo.js
+++ fundamentals/yoli-todo.js
@@ -31,4 +31,20 @@ app.controller('TodoCtrl', [function(){
  		td.todos.push({ text: td.newTask, done: false });
  		td.newTask = "";
  	};
+
+ 	td.updateList = function(){
+ 		//we make a shallow copy of the current state
+ 		var oldTodos = td.todos;
+ 		//we reset the state to empty 
+ 		td.todos = []
+ 		//ignore checked elements from the view, 
+ 		//and only show the todos.done = false
+ 		angular.forEach(oldTodos, function(item){
+ 			if(!item.done){
+ 				td.todos.push(item);
+ 			};
+ 		});
+ 		
+ 		
+ 	};
 }]);

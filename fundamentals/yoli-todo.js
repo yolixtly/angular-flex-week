eb38fb34b3a0f5b402f7ff0a7fd1dfdd8b9fce76
yolixtly <yolixtly_13@hotmail.com>
2016-10-09 13:19:17 -0400

    Feature added: Checkbox and counter working

--- fundamentals/yoli-todo.js
+++ fundamentals/yoli-todo.js
@@ -2,7 +2,28 @@ console.log('I am building an app I am proud of! I enjoy my journey in its devel
 
 var app = angular.module("yoliTodoApp", []);
 
-app.controller('HelloCtrl', function(){
+app.controller('HelloCtrl', [function(){
 	var vm = this;
 	vm.sayHi = "I know I can go a little further...";
-});
+}]);
+
+app.controller('TodoCtrl', [function(){
+	var td = this;
+
+ 	td.todos = [
+ 		 { "text": "Finish a Todo App with Angular", "done": false },
+ 		 { "text": "Add Validations for these entries", "done": false },
+ 		 { "text": "Add Node.js to this app", "done": false }
+ 	];
+
+ 	// td.reminding = function(){}
+ 	td.reminding = function(){
+ 		var count = 0; 
+ 		for(var i = 0; i < td.todos.length; i++){
+ 			if(!td.todos[i].done){
+ 				count++;
+  			}
+ 		}
+ 		return count; 
+ 	};
+}]);

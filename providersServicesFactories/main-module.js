4e671d3b977f99d39a25fe847b14463f197fde0a
yolixtly <yolixtly_13@hotmail.com>
2016-10-11 14:24:52 -0400

    similar functionality to Provider

--- providersServicesFactories/main-module.js
+++ providersServicesFactories/main-module.js
@@ -1,18 +1,35 @@
 var app = angular.module("myModule", []);
 
-app.provider("myProvider", function(){
-  //the first time that Angular calls this provider, the $get gets called only once. 
-  //Angular stores caches the value. and subsecuent calls to our provider return that stored value
-    this.$get = function(){
-    console.log("myProvider.$get() called");
+/*
+ PROVIDER:
+  - the first time that Angular calls this provider, the $get gets called only once. 
+  - Angular stores caches the value. and subsecuent calls to our provider return that 
+    stored value
+  - Requires to assign the value to pass, to a $get method
+ */
+// app.provider("myProvider", function(){
+//     this.$get = function(){
+//     console.log("myProvider.$get() called");
+//     return "my Value";
+//   };
+// });
+
+/*
+ Factory:
+  - we simply assign the value, without the need of a $get service
+  - in many cases what we return instead of a string, usually we pass an object
+ */
+
+
+app.factory("myFactory", function(){
+    console.log("MyFactory is called");
     return "my Value";
-  };
 });
 
-app.controller("FirstController", function(myProvider){
-  console.log("myProvider: " + myProvider);
+app.controller("FirstController", function(myFactory){
+  console.log("myFactory: " + myFactory);
 });
 
-app.controller("SecondController", function(myProvider){
-  console.log("myProvider: " + myProvider);
+app.controller("SecondController", function(myFactory){
+  console.log("myFactory: " + myFactory);
 });

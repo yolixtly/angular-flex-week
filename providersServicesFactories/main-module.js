b4b03c7ecc649ddb7ab344da1cf1c73430ada6d9
yolixtly <yolixtly_13@hotmail.com>
2016-10-11 14:31:00 -0400

    Factory returns objects, also only gets called once.

--- providersServicesFactories/main-module.js
+++ providersServicesFactories/main-module.js
@@ -18,18 +18,26 @@ var app = angular.module("myModule", []);
  Factory:
   - we simply assign the value, without the need of a $get service
   - in many cases what we return instead of a string, usually we pass an object
+  - like in an http request, what we return is the entire data or Object 
+        -that is the key difference Factory vs Service 
  */
 
+ function myObject(){
+  this.getValue = function(){
+    return "My Object Value";
+  };
+ }
 
 app.factory("myFactory", function(){
-    console.log("MyFactory is called");
-    return "my Value";
+    console.log("MyFactory function is called once");
+    return new myObject();
 });
 
 app.controller("FirstController", function(myFactory){
-  console.log("myFactory: " + myFactory);
+  console.log("myFactory: " + myFactory.getValue());
 });
 
 app.controller("SecondController", function(myFactory){
-  console.log("myFactory: " + myFactory);
+  console.log("myFactory: " + myFactory.getValue());
 });
+

336524ccea7092fb130312c7f26be74556d897b4
yolixtly <yolixtly_13@hotmail.com>
2016-10-13 21:23:19 -0400

    cleaning presentation

--- topic-example/resume/app.js
+++ topic-example/resume/app.js
@@ -2,7 +2,7 @@ var app = angular.module('myApp', ['ngRoute']);
 
 
 function explainMe($rootScope, $scope, type, explain){
-	// console.clear();
+	console.clear();
 	console.log(type, "========================");
 	console.log("setTimeout is triggered" + explain);
 	console.log("$rootScope.rootMessage: ", $rootScope.rootMessage);
--- topic-example/resume/directives/nav-bar-directive.js
+++ topic-example/resume/directives/nav-bar-directive.js
@@ -5,22 +5,6 @@ directive('navigationBar', function() {
         scope: {},
         transclude: true,
         templateUrl: './views/nav-bar.html',
-        // controller: './controllers/topics-controller.js'
-        controller: function($scope) {
-            $scope.appTitle = 'FlexWeek';
-            $scope.topics = [{
-                route: 'setTimeoutVanilla',
-                title: 'VanillaJS'
-            }, {
-                route: 'setTimeoutDigest',
-                title: '$digest'
-            }, {
-                route: 'setTimeoutApply',
-                title: '$apply'
-            }, {
-                route: 'setTimeoutService',
-                title: '$setTimeout'
-            }, ];
-        }
+        controller: 'Topics' //only pass the name
     };
 });
